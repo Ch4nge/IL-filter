f2346d4accbbef3f6ddb0bcd6c1cd87715bfc592
Ch4nge <sami.hautamaki1@gmail.com>
2023-06-01 01:30:43 +0300

    Add filtering

---
+++ content.js
@@ -0,0 +1,13 @@
+// Words to filter
+let filterWords = ['syöpä']; 
+
+let newsTitles = document.querySelectorAll('.front-title');
+
+for (let title of newsTitles) {
+  for (let word of filterWords) {
+    if (title.textContent.toLowerCase().includes(word.toLowerCase())) {
+      title.parentElement.style.display = 'none';
+      break;
+    }
+  }
+}
