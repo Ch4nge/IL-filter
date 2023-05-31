869ddcf8639c0d18e460f830f00f599eef5f9d95
Ch4nge <sami.hautamaki1@gmail.com>
2023-06-01 02:02:40 +0300

    Add support for is.fi also

--- content.js
+++ content.js
@@ -1,13 +1,28 @@
 // Words to filter
 let filterWords = ['syöpä']; 
 
-let newsTitles = document.querySelectorAll('.front-title');
+let newsTitlesIL = document.querySelectorAll('.front-title');
 
-for (let title of newsTitles) {
+for (let title of newsTitlesIL) {
   for (let word of filterWords) {
     if (title.textContent.toLowerCase().includes(word.toLowerCase())) {
       title.parentElement.parentElement.parentElement.style.display = 'none';
       break;
+    }
+  }
+}
+
+let articlesIS = document.getElementsByTagName('article');
+console.log(articlesIS);
+
+for (let article of articlesIS) {
+  const spans = article.getElementsByTagName('span');
+  for (let span of spans) {
+    for (let word of filterWords) {
+      if (span.textContent.toLowerCase().includes(word.toLowerCase())) {
+        article.style.display = 'none';
+        break;
+      }
     }
   }
 }
