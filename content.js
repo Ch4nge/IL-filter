c6154ca3dca127ed67a60c6b03c2f720e666d1dd
Ch4nge <sami.hautamaki1@gmail.com>
2023-06-01 01:35:53 +0300

    filter upper elements for prettier view

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ let newsTitles = document.querySelectorAll('.front-title');
 for (let title of newsTitles) {
   for (let word of filterWords) {
     if (title.textContent.toLowerCase().includes(word.toLowerCase())) {
-      title.parentElement.style.display = 'none';
+      title.parentElement.parentElement.parentElement.style.display = 'none';
       break;
     }
   }
